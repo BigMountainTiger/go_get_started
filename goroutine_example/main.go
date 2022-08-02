1a79ea5b863427ff2aba175d5f39f40d8080cc80
Song Li <da_tou_li@yahoo.com>
2022-08-02 18:13:03 -0400

    Updated the go_get_started

---
+++ goroutine_example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Println(r)
+			fmt.Println("Recovered")
+		}
+	}()
+
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
