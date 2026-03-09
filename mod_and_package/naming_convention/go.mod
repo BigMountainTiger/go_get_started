44d4729d43788bc65c0076b9c70e9bddca02586f
Song Li <da_tou_li@yahoo.com>
2026-03-09 10:13:49 -0400

    Working on mod_and_package

---
+++ mod_and_package/naming_convention/go.mod
@@ -0,0 +1,3 @@
+module song.com/go_get_started/package_example/naming_convention
+
+go 1.25.0
---
+++ mod_and_package/naming_convention/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	// the package name is different from the folder name
+	// It is allowed but not recommended, the strong convention is to have the package name the same as the folder name
+	// The import path is based on "the module name" / "the folder structure"
+	any_package "song.com/go_get_started/package_example/naming_convention/folder1"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// the package name is different from the folder name,
+	// but we can still call the function using the package name
+	any_package.Which_package()
+}
